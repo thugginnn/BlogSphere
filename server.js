cfffb031bba8cbbec0e94448f5416ea5242829d4
thugginnn <paul.iyiade4@gmail.com>
2024-07-23 15:06:37 +0100

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const sequelize = require('./config/database');
+
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
