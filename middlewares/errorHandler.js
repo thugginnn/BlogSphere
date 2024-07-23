18df79de53431f7621fa47be1ba79923b7cd51ef
thugginnn <paul.iyiade4@gmail.com>
2024-07-23 18:12:20 +0100

    err

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,12 @@
-module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-};
+const logger = require('../utils/logger');
+
+function errorHandler(err, req, res, next) {
+  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: process.env.NODE_ENV === 'development' ? err : {}  // Show detailed error only in development
+  });
+}
+
+module.exports = errorHandler;
