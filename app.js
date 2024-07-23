a6d4822d99af6d2623e09ad96645e0bb7f96f68d
thugginnn <paul.iyiade4@gmail.com>
2024-07-23 18:23:25 +0100

    app

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const errorHandler = require('./middlewares/errorHandler');
-const logger = require('./utils/logger');
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-	logger.info('Received request: ${req.method) ${req.url}');
-	next();
-});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
